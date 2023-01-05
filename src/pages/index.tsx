67453b3247d216f00be4a35572df887ec574ec0d
Ethan Bickel <notethanbickel@gmail.com>
2023-01-05 14:53:51 -0600

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import { api } from "../utils/api";
+
+const Home: NextPage = () => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default Home;
