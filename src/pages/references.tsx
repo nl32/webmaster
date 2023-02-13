0d1b610c4fda067ce38702084cccfdb1cdbd02cb
Ethan Bickel <notethanbickel@gmail.com>
2023-02-12 23:13:27 -0600

    fixed quot

--- src/pages/references.tsx
+++ src/pages/references.tsx
@@ -139,7 +139,7 @@ const References: NextPage = () => {
                   The MIT License (MIT) Copyright (c) 2014-2023 HackerOne Inc
                   and individual contributors Permission is hereby granted, free
                   of charge, to any person obtaining a copy of this software and
-                  associated documentation files (the &quot;Software&qout;), to
+                  associated documentation files (the &quot;Software&quot;), to
                   deal in the Software without restriction, including without
                   limitation the rights to use, copy, modify, merge, publish,
                   distribute, sublicense, and/or sell copies of the Software,
